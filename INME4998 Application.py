7663abfd268f678453bcc745b9ffb6ad6d9ac668
AlexanderPerez11 <perezampr@gmail.com>
2020-05-01 20:12:11 -0400

    Initial commit

--- INME4998 Application.py
+++ INME4998 Application.py
@@ -1,1 +1,11 @@
+import pygame as pg
 
+def main(running):
+    running = True
+    while running:
+        print("This application takes in two sets of information from a six degree of freedom IMU unit and process the \n"
+              "data to produce an animation in a 3-D space created with Open GL for an easy to interpret visualization \n"
+              "of the sensor data")
+        a = input()
+        if a == "e":
+            running = False
