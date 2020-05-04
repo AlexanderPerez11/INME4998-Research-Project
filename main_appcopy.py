bb0718aed8d71fad799e443b95fe8be5a18d3c23
AlexanderPerez11 <perezampr@gmail.com>
2020-05-04 13:27:56 -0400

    Initial commit

--- main_appcopy.py
+++ main_appcopy.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
 import pygame_textinput
 import SensorDataProcessingModule
 import os
-os.environ['SDL_VIDEO_WINDOW_POS'] = '400,200'
 from OpenGL.GL import *
 from OpenGL.GL.shaders import compileProgram, compileShader
 import pygame
@@ -12,16 +10,68 @@ from TextureLoader import load_texture_pygame
 from ObjLoader import ObjLoader
 from camera import Camera
 
+os.environ['SDL_VIDEO_WINDOW_POS'] = '400,200'
+
+# Write out the shader program source code in Open GL C++
+vertex_src = """
+# version 330
+layout(location = 0) in vec3 a_position;
+layout(location = 1) in vec2 a_texture;
+layout(location = 2) in vec3 a_normal;
+
+
+uniform mat4 model;
+uniform mat4 projection;
+uniform mat4 view;
+out vec2 v_texture;
+
+void main()
+{
+    gl_Position = projection * view * model * vec4(a_position, 1.0);
+    v_texture = a_texture;
+
+}
+"""
+
+fragment_src = """
+# version 330
+in vec2 v_texture;
+out vec4 out_color;
+uniform sampler2D s_texture;
+void main()
+{
+    out_color = texture(s_texture, v_texture);
+}
+"""
+
+
+def mouse_look(xpos, ypos):
+    global first_mouse, lastX, lastY
+
+    if first_mouse:
+        lastX = xpos
+        lastY = ypos
+        first_mouse = False
+
+    xoffset = xpos - lastX
+    yoffset = lastY - ypos
+
+    lastX = xpos
+    lastY = ypos
+
+    cam.process_mouse_movement(xoffset, yoffset)
+
 
 pygame.init()
+
 black = (0, 0, 0)
 white = (255, 255, 255)
 light_gray = (100, 100, 100)
 dark_gray = (50, 50, 50)
+
 screen_size = width, length = (1080, 520)
 
 filename = "Text"
-kinematic_data = None
 title_font = pygame.font.SysFont("Times New Roman", 24)
 general_font = pygame.font.SysFont('Times New Roman', 18)
 global action_1
@@ -31,6 +81,7 @@ cam = Camera()
 WIDTH, HEIGHT = 1280, 720
 lastX, lastY = WIDTH / 2, HEIGHT / 2
 first_mouse = True
+
 
 def blit_text(surface, text, pos, font, color=white):
     words = [word.split(' ') for word in text.splitlines()]  # 2D array where each row is a list of words.
@@ -90,9 +141,6 @@ def main():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 exit()
 
-        # Feed it with events every frame
-
-        # Blit its surface onto the screen
         blit_text(screen, title, (20, 20), title_font)
         blit_text(screen, instructions, (20, 40), general_font)
         blit_text(screen, options_1, (20, 300), general_font)
@@ -112,6 +160,7 @@ def main():
                 filename = textinput.get_text()
                 kinematic_data = np.array(SensorDataProcessingModule.process_data(filename))
                 typing = False
+
             if event.key == pygame.K_TAB:
                 animation()
                 screen = pygame.display.set_mode(screen_size)
@@ -166,28 +215,26 @@ def graphing_menu():
 
         pygame.display.update()
         clock.tick(30)
-    return action_1, kinematic_data
 
 
 def animation_menu():
     global filename
     global kinematic_data
-    running_1 = True
+    running = True
     screen = pygame.display.set_mode(screen_size)
     clock = pygame.time.Clock()
-    while running_1:
+    while running:
         screen.fill(black)
         events = pygame.event.get()
         for event in events:
             if event.type == pygame.QUIT:
                 exit()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-                running_1 = False
+                running = False
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RETURN:
                 pass
-
 
         textSurf, textRect = text_objects(str(filename), general_font)
         textRect.center = ((150 + (200 / 2)), (400 + (50 / 2)))
@@ -196,63 +243,10 @@ def animation_menu():
         pygame.display.update()
         clock.tick(30)
 
-# Import necessary libraries and dependencies
-
-
-
-# Write out the shader program source code in Open GL C++
-vertex_src = """
-# version 330
-layout(location = 0) in vec3 a_position;
-layout(location = 1) in vec2 a_texture;
-layout(location = 2) in vec3 a_normal;
-
-
-uniform mat4 model;
-uniform mat4 projection;
-uniform mat4 view;
-out vec2 v_texture;
-
-void main()
-{
-    gl_Position = projection * view * model * vec4(a_position, 1.0);
-    v_texture = a_texture;
-
-}
-"""
-
-fragment_src = """
-# version 330
-in vec2 v_texture;
-out vec4 out_color;
-uniform sampler2D s_texture;
-void main()
-{
-    out_color = texture(s_texture, v_texture);
-}
-"""
-
-
-def mouse_look(xpos, ypos):
-    global first_mouse, lastX, lastY
-
-    if first_mouse:
-        lastX = xpos
-        lastY = ypos
-        first_mouse = False
-
-    xoffset = xpos - lastX
-    yoffset = lastY - ypos
-
-    lastX = xpos
-    lastY = ypos
-
-    cam.process_mouse_movement(xoffset, yoffset)
-
 
 def animation():
     # Import position data for x y z coordinates
-    global kinematic_data
+    global kinematic_data, model
     x = kinematic_data[7][0]
     y = kinematic_data[8][0]
     z = kinematic_data[9][0]
@@ -261,16 +255,10 @@ def animation():
     cube_indices, cube_buffer = ObjLoader.load_model("meshes/Rubiks Cube.obj")
     grid_indices, grid_buffer = ObjLoader.load_model("meshes/uv grid floor.obj")
 
-    # Initialize a pg application
-    pygame.init()
-
     # Set up a pg display with Open GL buffer and double buff an d resizable tags
-
     pygame.display.set_mode((WIDTH, HEIGHT), pygame.OPENGL | pygame.DOUBLEBUF | pygame.RESIZABLE)
-    # size = width, height = (32, 32)
-    # empty_surface = pg.Surface(size)
-    # pg.draw.rect(empty_surface, (255, 0, 0), 10, 10)
-    # pg.mouse.set_visible(False)
+
+    pygame.mouse.set_visible(False)
     pygame.event.set_grab(True)
 
     # Compile the shader program with the source vertex and fragment codes
@@ -324,25 +312,18 @@ def animation():
     glEnable(GL_BLEND)
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
 
-    # Create all necessary matrices for projections, positions and views
-
     # Perspective, aspect ratio, near clipping plane, far clipping plane
     projection = pyrr.matrix44.create_perspective_projection_matrix(45, WIDTH / HEIGHT, 0.1, 50)
 
     grid_pos = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, -2, 0]))
 
-    # Position, target, up
-    # view = pyrr.matrix44.create_look_at(pyrr.Vector3([0, 2.5, 10]), pyrr.Vector3([0, 0, 0]), pyrr.Vector3([0, 1, 0]))
-
     # Call matrices locations in the shader source code
     model_loc = glGetUniformLocation(shader, "model")
     proj_loc = glGetUniformLocation(shader, "projection")
     view_loc = glGetUniformLocation(shader, "view")
-    # switcher_loc = glGetUniformLocation(shader, "switcher")
 
     # Upload static matrices to the shader program
     glUniformMatrix4fv(proj_loc, 1, GL_FALSE, projection)
-    # glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
 
     # Set up main application loop
     running = True
@@ -395,7 +376,6 @@ def animation():
         if keys_pressed[pygame.K_LCTRL]:
             cam.process_keyboard("DOWN", velocity)
 
-
         mouse_pos = pygame.mouse.get_pos()
 
         if mouse_pos[0] <= 0:
@@ -404,27 +384,17 @@ def animation():
             pygame.mouse.set_pos((0, mouse_pos[1]))
 
         mouse_look(mouse_pos[0], mouse_pos[1])
-        # ct = pg.time.get_ticks() / 1000
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
         view = cam.get_view_matrix()
         glUniformMatrix4fv(view_loc, 1, GL_FALSE, view)
-        # glUniform1i(switcher_loc, 0)
 
-        # rot_x = pyrr.Matrix44.from_x_rotation(theta_x[i])
-        # rot_y = pyrr.Matrix44.from_y_rotation(theta_y[i])
-        # rot_z = pyrr.Matrix44.from_z_rotation(theta_z[i])
-
-        # rotation = pyrr.matrix44.multiply(rot_x, rot_y)
-        # rotation = pyrr.matrix44.multiply(rotation, rot_z)
         if not do_move:
             model = pyrr.matrix44.create_from_translation(pyrr.Vector3([0, 0, 0]))
 
         elif do_move:
-            cube_translation = pyrr.matrix44.create_from_translation(pyrr.Vector3([x[i], y[i], z[i]]))
-            # model = pyrr.matrix44.multiply(rotation, cube_translation)
-            model = cube_translation
+            model = pyrr.matrix44.create_from_translation(pyrr.Vector3([x[i], y[i], z[i]]))
 
         glBindVertexArray(VAO[0])
         glBindTexture(GL_TEXTURE_2D, texture[0])
@@ -445,8 +415,6 @@ def animation():
         if do_move:
             if i < len(x) - 1:
                 i += 1
-
-
 
 
 main()
